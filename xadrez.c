dbdb93462845e21a76eb52f381b8c769484aad07
Lmartins7 <luismartins7797@gmail.com>
2025-05-10 17:37:35 +0000

    nivel_mestre

--- xadrez.c
+++ xadrez.c
@@ -7,42 +7,24 @@
 int main() {
     // Nível Novato - Movimentação das Peças
     // Sugestão: Declare variáveis constantes para representar o número de casas que cada peça pode se mover.
-    int bispo = 0, torre = 0, rainha = 0;
+    
 
     // Implementação de Movimentação do Bispo
     // Sugestão: Utilize uma estrutura de repetição para simular a movimentação do Bispo em diagonal.
-    while (bispo <5 ){
-        printf("Movimentação do bispo: Cima e Direita\n");
-        bispo++;
-    }
-    
+    bispo(1);
+
     // Implementação de Movimentação da Torre
     // Sugestão: Utilize uma estrutura de repetição para simular a movimentação da Torre para a direita.
-    while (torre < 5){
-        printf("Movimentação da torre: Direita\n");
-        torre++; 
-    }
+    torre(1);
     
     // Implementação de Movimentação da Rainha
     // Sugestão: Utilize uma estrutura de repetição para simular a movimentação da Rainha para a esquerda.
-    while (rainha < 8){
-        printf("Movimentação da rainha: Esquerda\n");
-        rainha++;
-    }
+    rainha(1);
     
     // Nível Aventureiro - Movimentação do Cavalo
     // Sugestão: Utilize loops aninhados para simular a movimentação do Cavalo em L.
     // Um loop pode representar a movimentação horizontal e outro vertical.
-    int cavalo=1;
-
-    while (cavalo,3){
-     for (int n = 1; n<=2; n++){
-        printf("Movimentação do cavalo: cima.\n");
-     }
-     printf("Movimentação do cavalo: direita.\n");
-     break;
-    }
-
+    cavalo(1);
     // Nível Mestre - Funções Recursivas e Loops Aninhados
     // Sugestão: Substitua as movimentações das peças por funções recursivas.
     // Exemplo: Crie uma função recursiva para o movimento do Bispo.
@@ -51,4 +33,34 @@ int main() {
     // Inclua o uso de continue e break dentro dos loops.
 
     return 0;
+}
+
+void bispo(int mov){
+    if (mov<=5){
+        printf("Bispo: Cima e Direita.\n");
+        bispo(mov + 1);
+    }
+}
+
+void torre(int move){
+    if(move<=5){
+        printf("Torre: Direita.\n");
+        torre(move + 1);
+    }
+}
+
+void rainha (int movi){
+    if(movi<=8){
+        printf("Rainha: Esquerda.\n");
+        rainha(movi + 1);
+    }
+}
+
+void cavalo (int mover){
+    if(mover<=3){
+        printf("Cavalo: Cima.\n");
+        cavalo(mover+1);
+    } else{
+        printf("Cavalo: Direita.\n");
+    }
 }
